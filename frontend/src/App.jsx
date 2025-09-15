10042672bff364afb4cf326487cdcea36f30ec45
imnotdeepak <rambutanandlycheejuice@gmail.com>
2025-09-15 19:33:52 -0400

    Frontend setup

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,35 +1,29 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import React from "react";
+import { Route, Routes } from "react-router";
+import ChatPage from "./pages/ChatPage";
+import LoginPage from "./pages/LoginPage";
+import SignUp from "./pages/SignUp";
+import { useAuthStore } from "./store/useAuthStore.js";
 
-function App() {
-  const [count, setCount] = useState(0)
-
+const App = () => {
+  const { authUser, isLoggedIn, login } = useAuthStore();
+  console.log(authUser, isLoggedIn);
   return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
-}
+    <div className="min-h-screen bg-black relative flex items-center justify-center p-4 overflow-hidden">
+      <div className="absolute inset-0 bg-[linear-gradient(to_right,#4f4f4f2e_1px,transparent_1px),linear-gradient(to_bottom,#4f4f4f2e_1px,transparent_1px)] bg-[size:14px_24px]" />
+      <div className="absolute top-0 -left-4 size-96 bg-[#6db0fc] opacity-20 blur-[100px]" />
+      <div className="absolute bottom-0 -right-4 size-96 bg-[#8ace00] opacity-20 blur-[100px]" />
 
-export default App
+      <button onClick={login} className="z-10">
+        Login
+      </button>
+      <Routes>
+        <Route path="/" element={<ChatPage />} />
+        <Route path="/login" element={<LoginPage />} />
+        <Route path="/signup" element={<SignUp />} />
+      </Routes>
+    </div>
+  );
+};
+
+export default App;
