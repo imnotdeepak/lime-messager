f978d900ed3060d294a2000edbc3d785305c9dad
imnotdeepak <rambutanandlycheejuice@gmail.com>
2025-09-16 15:25:49 -0400

    Added the login and logout functionality for the frontend with the correct routing

--- frontend/src/pages/ChatPage.jsx
+++ frontend/src/pages/ChatPage.jsx
@@ -1,7 +1,14 @@
 import React from "react";
+import { useAuthStore } from "../store/useAuthStore.js";
 
 const ChatPage = () => {
-  return <div>ChatPage</div>;
+  const { logout } = useAuthStore();
+
+  return (
+    <div className="z-10">
+      <button onClick={logout}>Logout</button>
+    </div>
+  );
 };
 
 export default ChatPage;
--- frontend/src/store/useAuthStore.js
+++ frontend/src/store/useAuthStore.js
@@ -5,6 +5,8 @@ import toast from "react-hot-toast";
 export const useAuthStore = create((set) => ({
   authUser: null,
   isCheckingAuth: true,
+  isSigningUp: false,
+  isLoggingIn: false,
 
   checkAuth: async () => {
     try {
@@ -29,6 +31,30 @@ export const useAuthStore = create((set) => ({
       toast.error(error.response.data.message);
     } finally {
       set({ isSigningUp: false });
+    }
+  },
+
+  login: async (data) => {
+    set({ isLoggingIn: true });
+
+    try {
+      const res = await axiosInstance.post("/auth/login", data);
+      set({ authUser: res.data });
+      toast.success("Login successful");
+    } catch (error) {
+      toast.error(error.response.data.message);
+    } finally {
+      set({ isLoggingIn: false });
+    }
+  },
+
+  logout: async () => {
+    try {
+      await axiosInstance.post("/auth/logout");
+      set({ authUser: null });
+      toast.success("Logged out successfully");
+    } catch (error) {
+      toast.error(error.response.data.message);
     }
   },
 }));
