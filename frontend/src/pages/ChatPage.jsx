2991fd5ae5033cae2faf3ce4b6666fb54ace4e62
imnotdeepak <rambutanandlycheejuice@gmail.com>
2025-09-16 16:54:38 -0400

    Added some styling to the chat page with the necessary components, no functionality

--- frontend/src/pages/ChatPage.jsx
+++ frontend/src/pages/ChatPage.jsx
@@ -1,12 +1,34 @@
 import React from "react";
 import { useAuthStore } from "../store/useAuthStore.js";
+import BorderAnimatedContainer from "../components/BorderAnimatedContainer.jsx";
+import { useChatStore } from "../store/useChatStore.js";
+import ProfileHeader from "../components/ProfileHeader.jsx";
+import ActiveTabSwitch from "../components/ActiveTabSwitch.jsx";
+import ChatsList from "../components/ChatsList.jsx";
+import ContactList from "../components/ContactList.jsx";
+import ChatContainer from "../components/ChatContainer.jsx";
+import NoCoversationPlaceholder from "../components/NoCoversationPlaceholder.jsx";
 
 const ChatPage = () => {
-  const { logout } = useAuthStore();
+  const { activeTab, selectedUser } = useChatStore();
 
   return (
-    <div className="z-10">
-      <button onClick={logout}>Logout</button>
+    <div className="relative w-full max-w-6xl h-[800px]">
+      <BorderAnimatedContainer>
+        {/* Left Side */}
+        <div className="w-80 bg-slate-800/50 backdrop-blue-sm flex flex-col">
+          <ProfileHeader />
+          <ActiveTabSwitch />
+
+          <div className="flex-1 overflow-y-auto p-4 space-y-2">
+            {activeTab === "chats" ? <ChatsList /> : <ContactList />}
+          </div>
+        </div>
+        {/* Right Side */}
+        <div className="flex-1 flex flex-col bg-slate-900/50 backdrop-blue-sm">
+          {selectedUser ? <ChatContainer /> : <NoCoversationPlaceholder />}
+        </div>
+      </BorderAnimatedContainer>
     </div>
   );
 };
